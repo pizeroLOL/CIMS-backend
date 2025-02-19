1c384407ec925a99cb742cea25f867e707a9bc6c
kaokao221 <kaokao221@outlook.com>
2025-02-20 01:43:21 +0800

    change profile sources, still in experimental, updated Jupyter Notebook.

--- CLI.py
+++ CLI.py
@@ -1,5 +1,7 @@
 import msvcrt
+import sys
 import os
+import json
 from string import whitespace
 
 # columns, lines = os.get_terminal_size()
@@ -66,3 +68,94 @@ MOUSE_DISAPPEAR = "\033?25l"
 MOUSE_APPEAR = "\033?25h"
 
 print(f"{CLEAR}{SET_MOUSE_PLACE(0,0)}Loading Server...")
+
+
+def list_clients():
+    with open("Datas/clients.json", "r") as clients_file:
+        return "\n".join([f"[{uid}]{id}" for uid, id in json.load(clients_file)])
+
+commands = [
+    {
+        "command": "ListClients",
+        "params": [],
+        "function": list_clients
+    },
+    # {
+    #     "command": "GetClientStatus",
+    #     "params": [
+    #         {
+    #             "choices": "ClientIDs",
+    #             "param": "ClineID"
+    #         }
+    #     ]
+    # }
+]
+
+
+print(CLEAR)
+
+
+def process_command_line(inputs:list[bytes], place:int) -> str:
+    if not (b"(" in inputs or b")" in inputs):
+        for command in commands:
+            if command["command"].startswith("".join([i.decode("utf-8") for i in inputs])):
+                return f"{CLEAR}{SET_MOUSE_PLACE(0, 0)}{"".join([i.decode("utf-8") for i in inputs])}{
+                WHITE_BACKGROUND + BLACK_CHARACTER}{command["command"][len(inputs):]}{BLACK_BACKGROUND + WHITE_CHARACTER}{SET_MOUSE_PLACE(0, place + 1)}"
+        return f"{CLEAR}{SET_MOUSE_PLACE(0, 0)}{"".join([i.decode("utf-8") for i in inputs])}{WHITE_BACKGROUND + BLACK_CHARACTER}???{BLACK_BACKGROUND + WHITE_CHARACTER}{SET_MOUSE_PLACE(0 ,place + 1)}"
+    return ""
+
+
+def command() -> list[bytes]:
+    inputs:list[bytes] = []
+    place:int = 0
+    while True:
+        match msvcrt.getch():
+            case b"\r":
+                return inputs
+
+            case b"\xe0":
+                match msvcrt.getch():
+                    case b"K":
+                        if place == 0:
+                            pass
+                        else:
+                            place -= 1
+
+                    case b"M":
+                        if place == len(inputs):
+                            pass
+                        else:
+                            place += 1
+
+                    case b"G":
+                        place = 0
+
+                    case b"O":
+                        place = len(inputs)
+
+                    case b"S":
+                        if place == len(inputs):
+                            pass
+                        else:
+                            del inputs[place]
+
+            case b"\x08":
+                if place == 0:
+                    pass
+                else:
+                    del inputs[place - 1]
+                    place -= 1
+
+            case b"\x03":
+                return [b"\x03"]
+
+            case _char:
+                inputs.insert(place, _char)
+                place += 1
+
+        sys.stdout.write(process_command_line(inputs, place))
+        sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    print(command())
--- ManagementServer/api.py
+++ ManagementServer/api.py
@@ -18,34 +18,71 @@ async def get_client_manifest(client_uid: str):
 
     :return 配置清单
     """
-    # 这里应该根据实际情况返回客户端的配置清单，这里只是一个示例
     manifest = {
-        "ServerKind": 1,
-        "OrganizationName": "示例组织",
+        "ClassPlanSource": {
+            "Value": f"http://127.0.0.1:50050/api/v1/client/{client_uid}/classPlan",
+            "Version": 1
+        },
+        "TimeLayoutSource": {
+            "Value": f"http://127.0.0.1:50050/api/v1/client/{client_uid}/timeLayout",
+            "Version": 1
+        },
+        "SubjectsSource": {
+            "Value": f"http://127.0.0.1:50050/api/v1/client/{client_uid}/subjects",
+            "Version": 1
+        },
+        "DefaultSettingsSource": {
+            "Value": f"http://127.0.0.1:50050/api/v1/client/{client_uid}/settings",
+            "Version": 1
+        },
         "PolicySource": {
-            "value": f"http://127.0.0.1:50050/api/v1/client/{client_uid}/policy",
-            "version": 2
-        }
-        # 其他配置...
+            "Value": f"http://127.0.0.1:50050/api/v1/client/{client_uid}/policy",
+            "Version": 2
+        },
+        "ServerKind": 1,
+        "OrganizationName": "CMS.py 本地测试"
     }
     return manifest
 
 
 @api.get("/api/v1/client/{client_uid}/policy", summary="获取策略")
 async def get_policy(client_uid: str):
-    """获取服务器策略（模拟）。"""
-    # 这里应该根据实际情况返回策略，这里只是一个示例
-    policy = {
-    "DisableProfileClassPlanEditing": True,
-    "DisableProfileTimeLayoutEditing": True,
-    "DisableProfileSubjectsEditing": True,
-    "DisableProfileEditing": True,
-    "DisableSettingsEditing": True,
-    "DisableSplashCustomize": True,
-    "DisableDebugMenu": True,
-    "AllowExitManagement": False
-}
+    """获取服务器策略"""
+    with open(f"Datas/Policies/{client_uid}.json", "r", encoding="utf-8") as f:
+        policy = json.load(f)
     return policy
+
+
+@api.get("/api/v1/client/{client_uid}/classPlan", summary="获取课表")
+async def get_class_plan(client_uid: str):
+    """获取服务器课表"""
+    with open(f"Datas/ClassPlans/{client_uid}.json", "r", encoding="utf-8") as f:
+        class_plan = json.load(f)
+    return class_plan
+
+
+@api.get("/api/v1/client/{client_uid}/subjects", summary="获取科目列表")
+async def get_subjects(client_uid: str):
+    """获取科目列表"""
+    with open(f"Datas/SubjectsSource/{client_uid}.json", "r", encoding="utf-8") as f:
+        subjects = json.load(f)
+    return subjects
+
+
+@api.get("/api/v1/client/{client_uid}/timeLayout", summary="获取时间表")
+async def get_time_layout(client_uid: str):
+    """获取时间表"""
+    with open(f"Datas/TimeLayouts/{client_uid}.json", "r", encoding="utf-8") as f:
+        time_layout = json.load(f)
+    return time_layout
+
+
+@api.get("/api/v1/client/{client_uid}/settings", summary="获取设置")
+async def get_settings(client_uid: str):
+    """获取设置"""
+    with open(f"Datas/DefaultSettings/{client_uid}.json", "r", encoding="utf-8") as f:
+        settings = json.load(f)
+    return settings
 
 async def start():
     """<UNK>FastAPI<UNK>"""
