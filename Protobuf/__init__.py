bb029144212a3cecb7a47a66b62958007a779a41
kaokao221 <kaokao221@outlook.com>
2025-03-22 19:42:00 +0800

    feat: 更正 Protobuf/__init__.py

--- Protobuf/__init__.py
+++ Protobuf/__init__.py
@@ -1,1 +1,10 @@
-from Protobuf import *
+from .Client import (ClientCommandDeliverScReq_pb2, ClientCommandDeliverScReq_pb2_grpc,
+                             ClientRegisterCsReq_pb2, ClientRegisterCsReq_pb2_grpc)
+from .Command import (SendNotification_pb2, SendNotification_pb2_grpc,
+                              HeartBeat_pb2, HeartBeat_pb2_grpc)
+from .Enum import (CommandTypes_pb2, CommandTypes_pb2_grpc,
+                           Retcode_pb2, Retcode_pb2_grpc)
+from .Server import (ClientCommandDeliverScRsp_pb2, ClientCommandDeliverScRsp_pb2_grpc,
+                             ClientRegisterScRsp_pb2, ClientRegisterScRsp_pb2_grpc)
+from .Service import (ClientCommandDeliver_pb2, ClientCommandDeliver_pb2_grpc,
+                              ClientRegister_pb2, ClientRegister_pb2_grpc)
